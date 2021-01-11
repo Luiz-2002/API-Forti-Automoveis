b813b65b165e6892f428f2edcfd554075bb148a6
LUIZ GUSTAVO CRUZ BASTOS <20185484@CFP127ALUNO.LOCAL>
2021-01-11 16:54:57 -0300

    Exercicio de projetos

---
+++ src/controllers/pedidos.js
@@ -0,0 +1,34 @@
+const Pedido = require("../models/Pedido");
+
+module.exports = {
+    //Função que vai ser executada pela rota
+    async listarPedidos (req, res) {
+
+        try {
+            const pedidos = await Pedido.findAll();
+    
+            res.send(pedidos);
+        } catch (error) {
+            console.log(error);
+            res.status(500).send({ error })
+        }
+    },
+
+    async adicionarPedidos (req, res) {
+        const { nome, produto, quantidade } = req.body;
+
+        try {
+
+            //SELECT * FROM alunos WHERE ra = ? AND email = ?
+
+                let pedido = await Pedido.create({ nome, produto, quantidade });
+
+            //retornar resposta de sucesso
+            res.status(201).send({pedido});
+        } catch (error) {
+            console.log(error);
+            res.status(500).send(error);
+        }
+    }
+
+}
